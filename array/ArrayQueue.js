c0a9e32defd62044dd98072bb3d2a589350cce5b
young <iezzuyoung@gmail.com>
2018-08-17 17:15:39 +0800

    feat: 由自己的数组实现队列

---
+++ array/ArrayQueue.js
@@ -0,0 +1,19 @@
+const {Array} = require('./Array');
+
+class ArrayQueue {
+    constructor (array = new Array()) {
+        this.array = array;
+    }
+
+    enqueue (e) {
+        return this.array.addLast(e);
+    }
+
+    dequeue () {
+        return this.array.removeFirst();
+    }
+
+    getFront () {
+        return this.array.getFirst();
+    }
+}
