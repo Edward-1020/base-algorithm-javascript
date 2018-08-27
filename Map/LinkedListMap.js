da593d9805c6534d84fafe767e24e01869192f4a
young <iezzuyoung@gmail.com>
2018-08-27 11:32:36 +0800

    feat: linkedlistMap

--- Map/LinkedListMap.js
+++ Map/LinkedListMap.js
@@ -25,6 +25,51 @@ class LinkedListMap {
 
         return null;
     }
+
+    get (key) {
+        let node = this.getNode(key);
+        return node === null ? null : node.value;
+    }
+
+    add (key, value) {
+        let node = this.getNode(key);
+
+        if (node === null) {
+            this.dummyHead.next = new _Node(key, value, this.dummyHead.next);
+            this.size++;
+        } else {
+            node.value = value;
+        }
+    }
+
+    set (key, newValue) {
+        let node = this.getNode(newValue);
+        if (node === null) {
+            throw new Error(`${key} doesn't exist!`);
+        }
+        node.value = newValue;
+    }
+
+    remove (key) {
+        let prev = this.dummyHead;
+
+        while (prev.next !== null) {
+            if (prev.next.key === key) {
+                break;
+            }
+
+            prev = prev.next;
+        }
+
+        if (prev.next !== null) {
+            let delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+            return delNode.value;
+        }
+
+        return null;
+    }
 }
 
 function _Node (key = null, value = null, next = null) {
